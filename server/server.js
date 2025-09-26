4ecbb4c2109adc416aee4e79669a23ead36a2356
Sam Louloudis <samanthaleelouloudis@gmail.com>
2025-09-26 10:02:57 +0100

    server and client

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json('THis is my server up and running!')
+})
 app.listen(8080, () => {
     console.log(`Server running on port 8080`)
 })
